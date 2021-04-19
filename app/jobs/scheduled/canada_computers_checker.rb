06acf3b1e1ecbb98b2d0e0b60ec015cca7795875
Neil Lalonde <neillalonde@gmail.com>
2021-04-19 17:25:14 -0400

    Watch for 5950x on Canada Computers and post in system message to me

---
+++ app/jobs/scheduled/canada_computers_checker.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+require "mechanize"
+
+module Jobs
+  class CanadaComputersChecker < ::Jobs::Scheduled
+
+    every 5.minutes
+
+    def execute(args = {})
+      check_5950x
+    end
+
+    def check_5950x
+      agent = Mechanize.new do |m|
+        m.user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"
+      end
+
+      r = agent.get("https://www.canadacomputers.com/product_info.php?ajaxstock=true&itemid=183427")
+
+      json = JSON.parse(r.body)
+
+      h = build_availability_hash(json)
+
+      online_count = online_available(h)
+      toronto_count = toronto_available(h)
+
+      if online_count > 0 || toronto_count > 0
+        notify_availability("5950x", build_availability_string(json))
+      elsif other_available?(h)
+        notify_availability("5950x-other", build_availability_string(json))
+      end
+    end
+
+    def build_availability_hash(json)
+      h = {}
+
+      (0..3).each do |i|
+        lkey = i == 0 ? "loc" : "loc#{i}"
+        akey = i == 0 ? "avail" : "avail#{i}"
+
+        if location = json[lkey]
+          h[location.gsub(" ", "_").downcase.to_sym] = json[akey] || "?"
+        end
+      end
+
+      h
+    end
+
+    def build_availability_string(json)
+      s = +""
+
+      (0..3).each do |i|
+        lkey = i == 0 ? "loc" : "loc#{i}"
+        akey = i == 0 ? "avail" : "avail#{i}"
+
+        if location = json[lkey]
+          s << "#{location}: #{json[akey] || '?'}\n\n"
+        end
+      end
+
+      s
+    end
+
+    def online_available(h)
+      if h[:online] && h[:online] != "NO AVAILABLE"
+        h[:online]
+      else
+        0
+      end
+    end
+
+    def toronto_available(h)
+      if toronto_key = h.keys.find { |k| k.to_s.include?("toronto") }
+        h[toronto_key]
+      else
+        0
+      end
+    end
+
+    def other_available?(h)
+      h.keys.any? { |k| k != :all_locations && k != :online }
+    end
+
+    def notify_availability(product_name, string)
+      tcf_name = "shopper-#{product_name}"
+
+      if tcf = TopicCustomField.joins(:topic).where(name: tcf_name).last
+        topic = tcf.topic
+        PostCreator.create!(
+          Discourse.system_user,
+          topic_id: topic.id,
+          raw: string,
+          skip_validations: true
+        )
+      else
+        post = SystemMessage.create_from_system_user(
+          User.where(username: "neil").first,
+          :shopper_found_availability,
+          product_name: product_name,
+          availability: string
+        )
+        topic = post.topic
+        topic.custom_fields[tcf_name] = "1"
+        topic.save
+        post
+      end
+    end
+  end
+end
---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# name: cpu-shopper
+# transpile_js: true
+# about: Shop for my new CPU for me.
+# version: 0.1
+# authors: Neil Lalonde
+# url: https://github.com/nlalonde/cpu-shopper
+
+enabled_site_setting :neil_cpu_shopper
+
+gem "mechanize", "2.8.0", require: false
+
+# after_initialize do
+
+# end
